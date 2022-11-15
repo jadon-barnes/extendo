887f06d620c2a25e578bfb815af25607c5219902
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-11-15 15:50:57 -0600

    Added NoiseFloat.cs

---
+++ Runtime/NoiseFloat.cs
@@ -0,0 +1,46 @@
+using Extendo.Utilities;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Extendo
+{
+	public class NoiseFloat : CustomUpdateBehaviour
+	{
+		public  bool              resetOnDisable;
+		private float             time;
+		public  float             speed       = 1f;
+		public  int               seed        = 12345;
+		public  Vector2           input       = new Vector2(0, 1);
+		public  Vector2           output      = new Vector2(0, 1);
+		public  Vector2           clampResult = new Vector2(0, 1);
+		public  UnityEvent<float> onNoiseUpdate;
+		public  float             Noise         { get; private set; }
+		public  float             NoiseRemapped { get; private set; }
+		public  float             NoiseResult   { get; private set; }
+
+		protected override void OnDisable()
+		{
+			base.OnDisable();
+
+			if (resetOnDisable)
+				time = 0f;
+		}
+
+		protected override void OnUpdate()
+		{
+			UpdateNoise();
+		}
+
+		private void UpdateNoise()
+		{
+			time          += Time.deltaTime * speed;
+			Noise         =  Mathf.PerlinNoise(time, seed);
+			NoiseRemapped =  Math.Remap(Noise, input, output);
+
+			NoiseResult = Mathf.Clamp(NoiseRemapped, clampResult.x, clampResult.y);
+
+			onNoiseUpdate.Invoke(NoiseResult);
+		}
+	}
+}
