7ac6a984fcda0a6e9432bf460d064498de4e9793
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-11-18 12:20:40 -0600

    Created timed update behaviour

---
+++ Runtime/TimedBehaviour.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Extendo
+{
+	public class TimedBehaviour : MonoBehaviour
+	{
+		public  bool      playOnEnable     = true;
+		public  bool      runOnFixedUpdate = false;
+		public  float     duration         = 2f;
+		public  float     ElapsedTime { get; private set; }
+		public  bool      Done        => AmountDone >= 1f;
+		public  float     AmountDone  => ElapsedTime / duration;
+		private Coroutine updateCoroutine;
+
+		protected virtual void OnEnable()
+		{
+			if (playOnEnable)
+				Start();
+		}
+
+		public void Start()
+		{
+			Stop();
+			ElapsedTime     = 0f;
+			updateCoroutine = StartCoroutine(UpdateRoutine());
+		}
+
+		public void Stop()
+		{
+			if (updateCoroutine != null)
+				StopCoroutine(updateCoroutine);
+		}
+
+		protected virtual void OnUpdate() {}
+
+		private IEnumerator UpdateRoutine()
+		{
+			while (!Done)
+			{
+				OnUpdate();
+				ElapsedTime += Time.deltaTime;
+				yield return runOnFixedUpdate ? new WaitForFixedUpdate() : null;
+			}
+
+			yield return null;
+		}
+	}
+}
