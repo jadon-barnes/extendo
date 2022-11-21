f2bdba7a22180a426596cd24ce035bd1f71d1144
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-11-21 12:20:35 -0600

    Added comments to Menu.cs

--- Runtime/Menus/Menu.cs
+++ Runtime/Menus/Menu.cs
@@ -6,11 +6,18 @@ namespace Extendo.Menus
 	[DisallowMultipleComponent]
 	public class Menu : MonoBehaviour
 	{
-		public  MenuAsset sampleMenu;
-		private Menu      parentMenu;
-		private Menu      childMenu;
-		public  bool      disableMenuOnChange;
-		public  bool      closeMenuOnBackIfNoParent = true;
+		/// <summary>
+		/// The parent <see cref="Menu"/> of this <see cref="Menu"/>.
+		/// </summary>
+		public Menu ParentMenu { get; private set; }
+		/// <summary>
+		/// The submenu of this <see cref="Menu"/>.
+		/// </summary>
+		public Menu ChildMenu { get; private set; }
+		[Tooltip("Disables the menu when it is not in focus.")]
+		public bool disableMenuOnChange;
+		[Tooltip("This will close the intial/root when 'GoBack()' is called, even if there is no menu to go back to.")]
+		public bool closeMenuOnBackIfNoParent = false;
 
 		public UnityEvent OnEnter;
 		public UnityEvent OnExit;
@@ -22,8 +29,8 @@ namespace Extendo.Menus
 		{
 			get
 			{
-				if (parentMenu)
-					return parentMenu.RootMenu;
+				if (ParentMenu)
+					return ParentMenu.RootMenu;
 
 				return this;
 			}
@@ -33,8 +40,8 @@ namespace Extendo.Menus
 		{
 			get
 			{
-				if (childMenu)
-					return childMenu.LastMenu;
+				if (ChildMenu)
+					return ChildMenu.LastMenu;
 
 				return this;
 			}
@@ -45,22 +52,32 @@ namespace Extendo.Menus
 			Close();
 		}
 
+		/// <summary>
+		/// Pass down data to a <see cref="ChildMenu"/>.
+		/// </summary>
 		public void SetDataPassedDown(object data)
 		{
 			dataPassedDown = data;
 		}
 
+		/// <summary>
+		/// Attempts to get data passed down from <see cref="ParentMenu"/>.
+		/// </summary>
+		/// <typeparam name="T">Data type</typeparam>
+		/// <returns>True if getting the data was successful.</returns>
 		public bool TryGetDataPassedIn<T>(out T data)
 		{
-			if (!parentMenu)
+			// No parent, return default
+			if (!ParentMenu)
 			{
 				data = default;
 				return false;
 			}
 
-			if (parentMenu.dataPassedDown is T)
+			// Get Data if is type
+			if (ParentMenu.dataPassedDown is T)
 			{
-				data = (T)parentMenu.dataPassedDown;
+				data = (T)ParentMenu.dataPassedDown;
 				return true;
 			}
 
@@ -68,32 +85,32 @@ namespace Extendo.Menus
 			return false;
 		}
 
-		[ContextMenu("New Test Menu")]
-		private void NewTestMenu()
-		{
-			GoToMenu(sampleMenu);
-		}
-
+		/// <summary>
+		/// Spawns the prefab from <see cref="MenuAsset"/> and assigns it as a <see cref="ChildMenu"/> of this <see cref="Menu"/>.
+		/// </summary>
 		public void GoToMenu(MenuAsset menuAsset)
 		{
-			if (childMenu)
+			if (ChildMenu)
 			{
-				Debug.LogWarning($"Child menu already spawned. Close {childMenu.gameObject.name} before going to a new menu.");
+				Debug.LogWarning($"Child menu already spawned. Close {ChildMenu.gameObject.name} before going to a new menu.");
 				return;
 			}
 
-			childMenu = menuAsset.SpawnMenu();
+			ChildMenu = menuAsset.SpawnMenu();
 
-			if (!childMenu)
+			if (!ChildMenu)
 			{
 				Debug.LogWarning("Can't find menu.");
 				return;
 			}
 
 			Exit();
-			childMenu.Enter(this);
+			ChildMenu.Enter(this);
 		}
 
+		/// <summary>
+		/// Jumps directly to this <see cref="Menu"/>.
+		/// </summary>
 		[ContextMenu("Go Here")]
 		public void GoHere()
 		{
@@ -101,6 +118,9 @@ namespace Extendo.Menus
 			DeleteChildren();
 		}
 
+		/// <summary>
+		/// Jumps directly to the initial/root <see cref="Menu"/>.
+		/// </summary>
 		[ContextMenu("Go Back To Start")]
 		public void GoBackToStart()
 		{
@@ -110,10 +130,13 @@ namespace Extendo.Menus
 				rootMenu.GoHere();
 		}
 
+		/// <summary>
+		/// Closes this menu and opens it's parent. Will NOT Close the initial/root <see cref="Menu"/> if <see cref="closeMenuOnBackIfNoParent"/> is set to false.
+		/// </summary>
 		[ContextMenu("Go Back")]
 		public void GoBack()
 		{
-			if (parentMenu)
+			if (ParentMenu)
 			{
 				Close();
 			}
@@ -122,17 +145,33 @@ namespace Extendo.Menus
 				Close();
 		}
 
+		/// <summary>
+		/// Closes/Destroys this <see cref="Menu"/> and enters the <see cref="ParentMenu"/>, destroying all <see cref="ChildMenu"/>s from this point downward.
+		/// </summary>
+		[ContextMenu("Close")]
+		public void Close()
+		{
+			// Invoke any events
+			OnClose.Invoke();
+
+			if (ParentMenu)
+				ParentMenu.Enter();
+
+			DeleteChildren();
+
+			Destroy(gameObject);
+		}
+
 		private void Enter(Menu parentMenu = null)
 		{
 			if (parentMenu)
-				this.parentMenu = parentMenu;
+				this.ParentMenu = parentMenu;
 
 			OnEnter.Invoke();
 
 			gameObject.SetActive(true);
 		}
 
-		[ContextMenu("Exit")]
 		private void Exit()
 		{
 			OnExit.Invoke();
@@ -141,29 +180,15 @@ namespace Extendo.Menus
 				gameObject.SetActive(false);
 		}
 
-		[ContextMenu("Close")]
-		public void Close()
-		{
-			// Invoke any events
-			OnClose.Invoke();
-
-			if (parentMenu)
-				parentMenu.Enter();
-
-			DeleteChildren();
-
-			Destroy(gameObject);
-		}
-
 		private void DeleteChildren()
 		{
 			// Destroy Children
-			if (!childMenu)
+			if (!ChildMenu)
 				return;
 
-			childMenu.DeleteChildren();
-			childMenu.OnClose.Invoke();
-			Destroy(childMenu.gameObject);
+			ChildMenu.DeleteChildren();
+			ChildMenu.OnClose.Invoke();
+			Destroy(ChildMenu.gameObject);
 		}
 
 		[ContextMenu("Disable Menu Group")]
@@ -172,6 +197,9 @@ namespace Extendo.Menus
 		[ContextMenu("Enable Menu Group")]
 		private void EnableMenuGroup() => EnableMenuGroup(true);
 
+		/// <summary>
+		/// Hides or reveals the entire <see cref="Menu"/> group. This is useful when you want to minimize or hide a group/collection of menus, retaining their context or state instead of completely destroying it.
+		/// </summary>
 		public void EnableMenuGroup(bool enable)
 		{
 			var lastMenu = LastMenu;
@@ -186,10 +214,10 @@ namespace Extendo.Menus
 		// TODO: Fix minimize and maximize
 		private void EnableParentMenus(bool enable)
 		{
-			if (parentMenu)
+			if (ParentMenu)
 			{
-				parentMenu.gameObject.SetActive(enable && !parentMenu.disableMenuOnChange);
-				parentMenu.EnableParentMenus(enable);
+				ParentMenu.gameObject.SetActive(enable && !ParentMenu.disableMenuOnChange);
+				ParentMenu.EnableParentMenus(enable);
 			}
 		}
 	}
