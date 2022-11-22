e7e4cbde36cc7e234f4fa4a972c0004c2e0faa97
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-11-21 18:24:14 -0600

    Update EventTimer.cs

--- Runtime/Events/EventTimer.cs
+++ Runtime/Events/EventTimer.cs
@@ -5,19 +5,51 @@ using UnityEngine.Events;
 namespace Extendo.Events
 {
 	[AddComponentMenu("Extendo/Events/Event Timer")]
-	public class EventTimer : TimeBehaviour
+	public class EventTimer : MonoBehaviour
 	{
+		public bool       resetOnEnable = true;
+		public float      duration      = 3f;
+		public bool       repeat;
+		public float      time;
+		public int        RepeatCountFromStart { get; private set; }
+		public float      TimeValue            => time / duration;
+		public bool       DurationReached      => time >= duration;
 		public UnityEvent onComplete;
-		public UnityEvent onRepeatComplete;
 
-		protected override void OnTimeComplete()
+		private void OnEnable()
 		{
-			onComplete.Invoke();
+			if (resetOnEnable)
+			{
+				Reset();
+			}
 		}
 
-		protected override void OnTimeRepeatComplete()
+		private void Update()
 		{
-			onRepeatComplete.Invoke();
+			if (DurationReached && !repeat)
+				return;
+
+			// Add Time
+			time = Mathf.Min(time + Time.deltaTime, duration);
+
+			if (DurationReached)
+			{
+				onComplete.Invoke();
+
+				// Repeat if applicable
+				if (repeat)
+				{
+					RepeatCountFromStart++;
+					time = 0;
+				}
+			}
+		}
+
+		[ContextMenu("Reset")]
+		public void Reset()
+		{
+			time                 = 0f;
+			RepeatCountFromStart = 0;
 		}
 	}
 }
