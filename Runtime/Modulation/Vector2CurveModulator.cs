54542e045f59cbe97a8c0c20d98f48a2541831dd
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-11-22 20:58:31 -0600

    Add curve modulators

---
+++ Runtime/Modulation/Vector2CurveModulator.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Extendo.Modulation
+{
+	public class Vector2CurveModulator : CurveModulator<Vector2>
+	{
+		public Vector2CurveModulator()
+		{
+			scale  = Vector2.one;
+			curveX = DefaultCurve;
+			curveY = DefaultCurve;
+		}
+
+		public AnimationCurve curveX, curveY;
+
+		protected override Vector2 GetValue()
+		{
+			return new Vector2
+			(
+				curveX.Evaluate(Mathf.Repeat(timer.TimeValue + offset.x, 1f)) * scale.x,
+				curveY.Evaluate(Mathf.Repeat(timer.TimeValue + offset.y, 1f)) * scale.y
+			);
+		}
+	}
+}
