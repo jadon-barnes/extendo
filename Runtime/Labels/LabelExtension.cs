6b72162e117ca0d3776dcb00c6cadd5624c5200c
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-11-29 10:14:18 -0600

    Rename params categories->labels

--- Runtime/Labels/LabelExtension.cs
+++ Runtime/Labels/LabelExtension.cs
@@ -4,24 +4,24 @@ namespace Extendo.Labels
 {
 	public static class LabelExtension
 	{
-		public static bool HasLabel(this Transform transform, params Label[] categories)
+		public static bool HasLabel(this Transform transform, params Label[] labels)
 		{
-			return Labeler.HasLabel(transform, categories);
+			return Labeler.HasLabel(transform, labels);
 		}
 
-		public static bool HasLabel(this Transform transform, params string[] categories)
+		public static bool HasLabel(this Transform transform, params string[] labels)
 		{
-			return Labeler.HasLabel(transform, categories);
+			return Labeler.HasLabel(transform, labels);
 		}
 
-		public static bool HasLabel(this GameObject gameObject, params Label[] categories)
+		public static bool HasLabel(this GameObject gameObject, params Label[] labels)
 		{
-			return Labeler.HasLabel(gameObject.transform, categories);
+			return Labeler.HasLabel(gameObject.transform, labels);
 		}
 
-		public static bool HasLabel(this GameObject gameObject, params string[] categories)
+		public static bool HasLabel(this GameObject gameObject, params string[] labels)
 		{
-			return Labeler.HasLabel(gameObject.transform, categories);
+			return Labeler.HasLabel(gameObject.transform, labels);
 		}
 	}
 }
