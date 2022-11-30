96cd9bf1e97863c3f1c23962d16f58390f3386ca
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-11-30 16:06:25 -0600

    Add Start and Stop to EventTimer

--- Runtime/Events/EventTimer.cs
+++ Runtime/Events/EventTimer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Extendo.Utilities;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,8 +8,10 @@ namespace Extendo.Events
 	[AddComponentMenu("Extendo/Events/Event Timer")]
 	public class EventTimer : MonoBehaviour
 	{
+		public                         bool       startOnEnable;
 		public                         bool       resetOnEnable = true;
-		[field: SerializeField] public Timer      Timer { get; private set; } = new(5f);
+		[field: SerializeField] public Timer      Timer    { get; private set; } = new(5f);
+		public                         bool       Counting { get; private set; }
 		public                         UnityEvent onDurationReached;
 
 		private void Awake()
@@ -20,11 +23,32 @@ namespace Extendo.Events
 		{
 			if (resetOnEnable)
 				Timer.Reset();
+
+			if (startOnEnable)
+				Start();
+		}
+
+		private void OnDisable()
+		{
+			Stop();
 		}
 
 		private void Update()
 		{
-			Timer.Update();
+			if (Counting)
+				Timer.Update();
+		}
+
+		[ContextMenu("Start")]
+		public void Start()
+		{
+			Counting = true;
+		}
+
+		[ContextMenu("Stop")]
+		public void Stop()
+		{
+			Counting = false;
 		}
 	}
 }
