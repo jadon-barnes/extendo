0d271812376606f64f1361faa1f84b92596ec052
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-12-03 12:55:50 -0600

    Add rotation limits

--- Runtime/Retargeting/DirectFollowRotation.cs
+++ Runtime/Retargeting/DirectFollowRotation.cs
@@ -5,9 +5,18 @@ namespace Extendo.Retargeting
 	[AddComponentMenu("Extendo/Retargeting/Direct Follow Rotation")]
 	public class DirectFollowRotation : FollowRotation
 	{
+		public bool lockX, lockY, lockZ;
+
+		private Vector3 LocalForward => transform.parent ? transform.parent.forward : Vector3.forward;
+		private Vector3 LocalUp      => transform.parent ? transform.parent.up : Vector3.up;
+
 		protected override Quaternion CalculateFollowValue()
 		{
-			return target.rotation;
+			var targetRotation = target.transform.forward;
+			targetRotation.x = lockY ? LocalForward.x : targetRotation.x;
+			targetRotation.y = lockX ? LocalForward.y : targetRotation.y;
+
+			return Quaternion.LookRotation(targetRotation, lockZ ? LocalUp : target.transform.up);
 		}
 	}
 }
