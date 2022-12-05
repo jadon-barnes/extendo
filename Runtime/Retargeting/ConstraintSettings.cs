a94f8c8d0d1b4c14d44c42abecabd7c51d4ad01e
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-12-05 13:45:28 -0600

    Update ConstraintSettings.cs

--- Runtime/Retargeting/ConstraintSettings.cs
+++ Runtime/Retargeting/ConstraintSettings.cs
@@ -6,7 +6,7 @@ namespace Extendo.Retargeting
 	[Serializable]
 	public class ConstraintSettings
 	{
-		public bool    useLocal = true;
+		public bool    useLocal = false;
 		public Vector3 offset;
 		public bool    enableX = true;
 		public bool    enableY = true;
