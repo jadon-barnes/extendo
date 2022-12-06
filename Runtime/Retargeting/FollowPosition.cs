f8dc50c6ad4d7f468df654a33f288e8f52063af9
Jadon Barnes <48486387+xJadonx@users.noreply.github.com>
2022-12-05 19:22:42 -0600

    Combine scripts

--- Runtime/Retargeting/FollowPosition.cs
+++ Runtime/Retargeting/FollowPosition.cs
@@ -1,11 +1,21 @@
+using System;
 using UnityEngine;
+using Math = Extendo.Utilities.Math;
 
 namespace Extendo.Retargeting
 {
-	public abstract class FollowPosition : FollowBehaviour
+	[AddComponentMenu("Extendo/Follow Position")]
+	public class FollowPosition : FollowBehaviour
 	{
-		public  ConstraintSettings constraintSettings = new ConstraintSettings();
-		private Vector3            followPosition;
+		public  InterpolationMethod interpolationMethod = InterpolationMethod.None;
+		public  ConstraintSettings  constraintSettings  = new ConstraintSettings();
+		private Vector3             followPosition;
+
+		public ExponentialDampSettings exponentialDampSettings;
+		public SmoothDampSettings      smoothDampSettings;
+		public SpringSettings          springSettings;
+
+		private Vector3 velocity;
 
 		protected override void CalculateFollow()
 		{
@@ -27,6 +37,30 @@ namespace Extendo.Retargeting
 			transform.position = followPosition;
 		}
 
-		protected abstract Vector3 CalculateFollowPosition(Vector3 from, Vector3 to);
+		protected Vector3 CalculateFollowPosition(Vector3 from, Vector3 to)
+		{
+			switch (interpolationMethod)
+			{
+				case InterpolationMethod.ExponentialDamp:
+					return Math.ExpDamp(from, to, exponentialDampSettings.smoothTime);
+				case InterpolationMethod.SmoothDamp:
+					return Vector3.SmoothDamp(
+						from,
+						to,
+						ref velocity,
+						smoothDampSettings.smoothTime,
+						smoothDampSettings.maxSpeed
+					);
+				case InterpolationMethod.Spring:
+					return Math.Spring(
+						from,
+						to,
+						ref velocity,
+						springSettings.springStrength,
+						springSettings.springDamp
+					);
+				default: return to;
+			}
+		}
 	}
 }
