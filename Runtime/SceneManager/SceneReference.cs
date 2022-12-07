e94207507a2352726d024f23365cb92c55fead32
Jadon Barnes <48486387+jadon-barnes@users.noreply.github.com>
2022-12-07 16:25:33 -0600

    Add scene reference scriptable object

---
+++ Runtime/SceneManager/SceneReference.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Extendo.SceneManager
+{
+	[CreateAssetMenu(fileName = "Scene Reference", menuName = "Extendo/Scene Reference")]
+	public class SceneReference : ScriptableObject
+	{
+		public int index;
+	}
+}
