fc6c71077bd18373f292439bd314e146287ffb29
Jadon Barnes <48486387+jadon-barnes@users.noreply.github.com>
2023-01-19 16:07:36 -0600

    Remove redundant audio extension method

--- Runtime/Audio/AudioExtension.cs
+++ Runtime/Audio/AudioExtension.cs
@@ -6,12 +6,6 @@ namespace Extendo.Audio
 {
 	public static class AudioExtension
 	{
-		public static void PlayRandom(this AudioSource audioSource, AudioClip[] audioClips)
-		{
-			audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
-			audioSource.Play();
-		}
-
 		public static void RandomizeVolume(this AudioSource audioSource, float min, float max)
 		{
 			audioSource.volume = Random.Range(max, max);
