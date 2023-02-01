bae4abc2d8d2c0bda3b31b55554b3a58892b4da7
Jadon Barnes <48486387+jadon-barnes@users.noreply.github.com>
2023-02-01 16:45:37 -0600

    Add virtual methods to FocusTarget

--- Runtime/FocusSystem/FocusTarget.cs
+++ Runtime/FocusSystem/FocusTarget.cs
@@ -3,6 +3,7 @@ using UnityEngine.Events;
 
 namespace Extendo.FocusSystem
 {
+	[DefaultExecutionOrder(-1)]
 	public class FocusTarget : MonoBehaviour
 	{
 		public UnityEvent onFocus;
@@ -10,5 +11,20 @@ namespace Extendo.FocusSystem
 		public bool       InFocus => FocusDirector.InFocus(this);
 
 		public FocusDirector GetFocusDirector() => FocusDirector.GetDirectorFromTarget(this);
+
+		protected virtual void Awake()
+		{
+			onFocus.AddListener(OnFocus);
+			onLostFocus.AddListener(OnLostFocus);
+		}
+
+		protected virtual void OnDestroy()
+		{
+			onFocus.RemoveListener(OnFocus);
+			onLostFocus.RemoveListener(OnLostFocus);
+		}
+
+		protected virtual void OnFocus() { }
+		protected virtual void OnLostFocus() { }
 	}
 }
